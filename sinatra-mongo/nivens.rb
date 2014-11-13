c878b94d9f26ca0d62b8865b2488e061413d8547
Wendy Smoak <wsmoak@gmail.com>
2014-11-13 11:49:19 -0500

    Change method of connecting to Mongo according to Heroku docs

--- sinatra-mongo/nivens.rb
+++ sinatra-mongo/nivens.rb
@@ -16,8 +16,20 @@ require 'sinatra'
 require 'mongo'
 require 'json/ext'
 require 'thin'
+require 'uri'
 
-db = Mongo::Connection.new.db("nivens")
+# see https://devcenter.heroku.com/articles/mongohq#adding-a-compose-database
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.password.nil?)
+  @db_connection
+end
+
+db = get_connection
+#db = Mongo::Connection.new.db("nivens")
 
 # collections
 rabbits = db["rabbits"]
